83ac196f97ab20e551057932ac6abb686e56671c
ilya5006 <samarcev.ilya@mail.ru>
2019-11-09 18:05:47 +0300

    Filling the board with colors

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+let initAndFillColorsCells = (field) =>
+{
+    let white = '#faf8dd';
+    let brown = '#662c17';
+    
+    let evenColor = brown; // Чётное
+    let oddColor = white; // Нечётное
+    
+    for (let i = 1; i <= 64; i++)
+    {
+        let oneCell = document.createElement('div');
+    
+        if (i % 2 == 1)
+            oneCell.style.backgroundColor = oddColor;
+        else
+            oneCell.style.backgroundColor = evenColor;
+    
+        if (i % 8 == 0) [evenColor, oddColor] = [oddColor, evenColor];
+        
+        field.insertAdjacentElement('beforeEnd', oneCell);
+    }
+}
+
 let field = document.querySelector('#field');
 
-for (let i = 1; i <= 64; i++)
-{
-    let oneCell = document.createElement('div');
-    field.insertAdjacentElement('beforeEnd', oneCell);
-}
+initAndFillColorsCells(field);
