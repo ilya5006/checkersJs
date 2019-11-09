68b037cdd7fabc583705592cb79159c8ae9ef97b
ilya5006 <samarcev.ilya@mail.ru>
2019-11-09 19:36:24 +0300

    Add setChecker function and switch rgb with hex

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+let setChecker = (oneCell, checker) =>
+{
+    oneCell.style.backgroundImage = `url("./img/${checker}.png")`;
+    oneCell.style.backgroundSize = 'cover';
+    oneCell.style.backgroundRepeat = 'no-repeat';
+}
+
 let initAndFillColorsCells = (field) =>
 {
-    let white = '#faf8dd';
-    let brown = '#662c17';
-    
+    let white = 'rgb(250, 248, 221)';
+    let brown = 'rgb(102, 44, 23)';
+
     let evenColor = brown; // Чётное
     let oddColor = white; // Нечётное
     
@@ -14,7 +21,12 @@ let initAndFillColorsCells = (field) =>
             oneCell.style.backgroundColor = oddColor;
         else
             oneCell.style.backgroundColor = evenColor;
-    
+
+        if (oneCell.style.backgroundColor == brown && i <= 24)
+            setChecker(oneCell, 'white');
+        if (oneCell.style.backgroundColor == brown && i >= 41)
+            setChecker(oneCell, 'black');
+
         if (i % 8 == 0) [evenColor, oddColor] = [oddColor, evenColor];
         
         field.insertAdjacentElement('beforeEnd', oneCell);
