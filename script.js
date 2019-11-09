b1499b4afda1d9bf87dfaf59a21b7b5bffd5b360
ilya5006 <samarcev.ilya@mail.ru>
2019-11-09 17:36:57 +0300

    Delete excess comment

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let field = document.querySelector('#field');
 
-// Creating cells and events for change background
 for (let i = 1; i <= 64; i++)
 {
     let oneCell = document.createElement('div');
